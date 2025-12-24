2f323c302985c69665e34569174df4db22f2b0c8
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2025-12-24 12:43:58 +0100

    [Add] fonction my_put_nbr

---
+++ day3/my_put_nbr.c
@@ -0,0 +1,16 @@
+void my_putchar(char c);
+
+int my_put_nbr(int nb){
+    if (nb < 0) {
+        my_putchar('-');
+		my_putchar(nb = -nb);
+    } if (nb > 0 && nb < 10) {
+        my_putchar((nb / 10) + (nb % 10) + '0');
+    } if (nb >= 10){
+		my_put_nbr(nb / 10);
+		my_putchar((nb % 10) + '0');
+	} if (nb == 0) {
+		my_putchar(nb + '0');
+	}
+    return (0);
+}
