a23a0a1270dce1541019ecc401b9754afa693750
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2025-12-28 18:46:30 +0100

    [Fix] my_put_nbr simplification

--- day3/my_put_nbr.c
+++ day3/my_put_nbr.c
@@ -3,14 +3,22 @@ void my_putchar(char c);
 int my_put_nbr(int nb){
     if (nb < 0) {
         my_putchar('-');
-		my_putchar(nb = -nb);
-    } if (nb > 0 && nb < 10) {
-        my_putchar((nb / 10) + (nb % 10) + '0');
-    } if (nb >= 10){
+		nb = -nb;
+    }
+	if (nb >= 10){
 		my_put_nbr(nb / 10);
-		my_putchar((nb % 10) + '0');
-	} if (nb == 0) {
-		my_putchar(nb + '0');
 	}
+	my_putchar((nb % 10) + '0');
     return (0);
 }
+
+
+//#include <unistd.h>
+
+//void my_putchar(char c) {
+//	write(1, &c, 1);
+//}
+//int main(void)
+//{
+//	my_put_nbr(-2147483648);
+//{
